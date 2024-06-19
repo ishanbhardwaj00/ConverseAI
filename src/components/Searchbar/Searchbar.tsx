684f5aae5787c0cd5bd98375fc70696ef3a75249
Ishan Bhardwaj <Ishan.Bhardwaj@hsc.com>
2024-06-19 12:28:31 +0530

    search bar

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -3,7 +3,7 @@ import './Searchbar.css'
 const Searchbar = () => {
   return (
     <div className="searchbar">
-      <input className="searchbar-input" type="text" />
+      <input className="searchbar-input" type="text" placeholder='Ask us something'/>
     </div>
   )
 }
