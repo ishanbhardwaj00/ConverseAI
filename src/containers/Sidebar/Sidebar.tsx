fccf5c084b7ef9aeba01a56644c35c566dc09272
Ishan Bhardwaj <Ishan.Bhardwaj@hsc.com>
2024-06-24 15:48:25 +0530

    menu button changed in sodebar

--- src/containers/Sidebar/Sidebar.tsx
+++ src/containers/Sidebar/Sidebar.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { RiMenuFill } from "react-icons/ri";
+import React from "react";
+import { RiMenuFill, RiCloseLargeFill } from "react-icons/ri";
+import Hamburger from "hamburger-react";
+
 import { MdOutlineClose } from "react-icons/md";
-import './Sidebar.css'
+import "./Sidebar.css";
 
 interface SidebarProps {
-    isSidebarOpen: boolean;
-    toggleSidebar: () => void;
-  }
+  isSidebarOpen: boolean;
+  toggleSidebar: () => void;
+}
 
 const Sidebar: React.FC<SidebarProps> = ({ isSidebarOpen, toggleSidebar }) => {
-
   return (
-    <div className={`sidebar-container ${isSidebarOpen ? 'open' : 'closed'}`}>
-        <button onClick={toggleSidebar} className="toggle-button">
-          {isSidebarOpen ? <MdOutlineClose /> : <RiMenuFill />}
-        </button>
-      <div className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
+    <div className={`sidebar-container ${isSidebarOpen ? "open" : "closed"}`}>
+      <Hamburger size={24} toggled={isSidebarOpen} toggle={toggleSidebar} />
+      {/* <div onClick={toggleSidebar} className="toggle-button">
+        {isSidebarOpen ? <VscMenu size={28} /> : <VscMenu size={28} />}
+      </div> */}
+      <div className={`sidebar ${isSidebarOpen ? "open" : "closed"}`}>
         <p>This is the sidebar content</p>
       </div>
     </div>
