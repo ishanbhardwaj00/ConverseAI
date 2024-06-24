83f4fe77d7e5d06eea09e1001e503f2a6a84ad3e
Ishan Bhardwaj <Ishan.Bhardwaj@hsc.com>
2024-06-24 12:50:44 +0530

    Loading added for messages, async comms

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -1,71 +1,97 @@
-import Loading from '../../components/Loading';
-import Message from '../../components/Message/Message';
-import NewChat from '../../components/NewChat/NewChat';
+import Loading from "../../components/Loading";
+import Message from "../../components/Message/Message";
+import NewChat from "../../components/NewChat/NewChat";
 import { IoFlaskOutline } from "react-icons/io5";
-import { useState } from 'react';
+import { useState } from "react";
 
-import './Chat.css'
-import axios from 'axios';
+import "./Chat.css";
+import axios from "axios";
 
-type MessageType =  {
-  message: string
-}
+type MessageType = {
+  message: string;
+};
 
 const Chat = () => {
-  const [messages, setMessages] = useState([{
-    message: "hello fvij'vwji;vvwjvwr;vw;rivwrjlvwrvliwrjvlviwrjvwlvjwrlvwrjv;lrvjwrilvrvlwir;jvwrlvjrwlvwrjvliwrvjr;lvwrj;vrwvil"
-  }, {
-    message: "hello, how can i assist you?"
-  }]);
+  const [messages, setMessages] = useState([
+    {
+      message:
+        "hello fvij'vwji;vvwjvwr;vw;rivwrjlvwrvliwrjvlviwrjvwlvjwrlvwrjv;lrvjwrilvrvlwir;jvwrlvjrwlvwrjvliwrvjr;lvwrj;vrwvil",
+    },
+    {
+      message: "hello, how can i assist you?",
+    },
+  ]);
 
   // let messages :MessageType[] = [];
   const [searchQuery, setSearchQuery] = useState("");
-
+  const [loading, setLoading] = useState(false);
   async function getAnswer(question: string) {
     console.log("gETTING Nawer");
-    
+    setLoading(true);
     const prompt = question.trim();
-    const response = await axios.post('http://192.168.251.212:5555/v1/completions', {
-      model: "/Hard_Disk-2/coe_codestral",
-      prompt: prompt,
-      max_tokens: 256,
-      temperature: 0.2
-  });
-
-
-  console.log(response.data.choices[0].text);
-  
+    const response = await axios.post(
+      "http://192.168.251.212:5555/v1/completions",
+      {
+        model: "/Hard_Disk-2/coe_codestral",
+        prompt: prompt,
+        max_tokens: 256,
+        temperature: 0.2,
+      }
+    );
+    setLoading(false);
+    console.log(response.data.choices[0].text);
   }
-  function appendMessage(message:string) {
+  function appendMessage(message: string) {
     console.log("aeending mesggse");
-    
+
     const msgs = messages;
-    msgs.push({message});
+    msgs.push({ message });
     setMessages(msgs);
   }
-  if(messages.length === 0) {
-    return <NewChat />
+  if (messages.length === 0) {
+    return <NewChat />;
   }
   return (
     <>
-    <div className="chat">
-      {messages.length === 0 ? <Loading />:  messages.map((message, index) => {
-        if(index&1)
-          return <div>{message.message}, {index}</div>
-        else
-          return <Message message={message.message} />
-          
-      })}
-    </div>
-    <div className="searchbar">
-      <div className="search-container">
-          <input value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} type="text" placeholder='Ask us something'/>
-          <IoFlaskOutline className='search-icon' size={28} onClick={() => {appendMessage(searchQuery); setSearchQuery("");}}/>
+      <div className="chat">
+        {messages.length === 0 ? (
+          <Loading />
+        ) : (
+          messages.map((message, index) => {
+            if (index & 1)
+              return (
+                <div>
+                  {message.message}, {index}
+                </div>
+              );
+            else return <Message message={message.message} />;
+          })
+        )}
+        {loading && <Loading />}
       </div>
-
-    </div>
+      <div className="searchbar">
+        <div className="search-container">
+          <input
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            type="text"
+            placeholder="Ask us something"
+          />
+          <IoFlaskOutline
+            className="search-icon"
+            size={28}
+            onClick={() => {
+              appendMessage(searchQuery);
+              setSearchQuery("");
+              getAnswer(searchQuery);
+            }}
+          />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
