9db9d16e232e350b0a5f6de27f12737c733b0bcf
Ishan Bhardwaj <Ishan.Bhardwaj@hsc.com>
2024-06-24 11:50:06 +0530

    main-screen

--- src/containers/Chat/Chat.tsx
+++ src/containers/Chat/Chat.tsx
@@ -1,24 +1,53 @@
 import Loading from '../../components/Loading';
 import Message from '../../components/Message/Message';
 import NewChat from '../../components/NewChat/NewChat';
+import { IoFlaskOutline } from "react-icons/io5";
+import { useState } from 'react';
+
 import './Chat.css'
+import axios from 'axios';
 
 type MessageType =  {
   message: string
 }
 
 const Chat = () => {
-  let messages:MessageType[] = [{
-    message: "hello"
+  const [messages, setMessages] = useState([{
+    message: "hello fvij'vwji;vvwjvwr;vw;rivwrjlvwrvliwrjvlviwrjvwlvjwrlvwrjv;lrvjwrilvrvlwir;jvwrlvjrwlvwrjvliwrvjr;lvwrj;vrwvil"
   }, {
     message: "hello, how can i assist you?"
-  }];
-  // let messages :MessageType[] = [];
+  }]);
 
+  // let messages :MessageType[] = [];
+  const [searchQuery, setSearchQuery] = useState("");
+
+  async function getAnswer(question: string) {
+    console.log("gETTING Nawer");
+    
+    const prompt = question.trim();
+    const response = await axios.post('http://192.168.251.212:5555/v1/completions', {
+      model: "/Hard_Disk-2/coe_codestral",
+      prompt: prompt,
+      max_tokens: 256,
+      temperature: 0.2
+  });
+
+
+  console.log(response.data.choices[0].text);
+  
+  }
+  function appendMessage(message:string) {
+    console.log("aeending mesggse");
+    
+    const msgs = messages;
+    msgs.push({message});
+    setMessages(msgs);
+  }
   if(messages.length === 0) {
     return <NewChat />
   }
   return (
+    <>
     <div className="chat">
       {messages.length === 0 ? <Loading />:  messages.map((message, index) => {
         if(index&1)
@@ -28,6 +57,14 @@ const Chat = () => {
           
       })}
     </div>
+    <div className="searchbar">
+      <div className="search-container">
+          <input value={searchQuery} onChange={(e) => { setSearchQuery(e.target.value) }} type="text" placeholder='Ask us something'/>
+          <IoFlaskOutline className='search-icon' size={28} onClick={() => {appendMessage(searchQuery); setSearchQuery("");}}/>
+      </div>
+
+    </div>
+    </>
   )
 }
 
--- src/containers/ChatScreen/ChatScreen.tsx
+++ src/containers/ChatScreen/ChatScreen.tsx
@@ -1,13 +1,11 @@
 import ChatHeading from "../../components/ChatHeading/ChatHeading";
 import Disclaimer from "../../components/Disclaimer/Disclaimer";
-import Searchbar from "../../components/Searchbar/Searchbar";
 import Chat from "../Chat/Chat";
 import './ChatScreen.css'
 const ChatScreen = () => {
   return <div className="main-chat-screen">
     <ChatHeading />
     <Chat />
-    <Searchbar />
     <Disclaimer />
     </div>;
 };
